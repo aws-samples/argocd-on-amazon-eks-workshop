992179c91b68d6279d53aef22897e140323263fb
Carlos Santana <csantana23@gmail.com>
2023-10-02 11:19:55 -0400

    delete ui load balancer on cleanup
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- terraform/spokes/destroy.sh
+++ terraform/spokes/destroy.sh
@@ -29,6 +29,12 @@ echo "Destroying $env ..."
 set -x
 
 terraform workspace select $env
+# Delete the Ingress/SVC before removing the addons
+TMPFILE=$(mktemp)
+terraform output -raw configure_kubectl > "$TMPFILE"
+source "$TMPFILE"
+kubectl delete svc --all -n ui
+
 terraform destroy -target="module.gitops_bridge_bootstrap" -auto-approve -var-file="workspaces/${env}.tfvars"
 terraform destroy -target="module.eks_blueprints_addons" -auto-approve -var-file="workspaces/${env}.tfvars"
 terraform destroy -target="module.eks" -auto-approve -var-file="workspaces/${env}.tfvars"
